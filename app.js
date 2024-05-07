81d0ce29f93f5bd861252538ae393ba6afea77f4
Denisha Kyla Azzahra <denishazzahra@gmail.com>
2024-05-07 21:37:31 +0700

    modified access

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 //ambil router yang mengandle endpoint user
 const userRouter = require('./routes/user');
 
